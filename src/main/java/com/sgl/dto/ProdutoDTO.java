32298ce636b93ae010c3b70e066fee3b1b6080cf
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-05 14:27:25 -0300

    fix: remover dto de descarte de ProdutoDTO

--- src/main/java/com/sgl/dto/ProdutoDTO.java
+++ src/main/java/com/sgl/dto/ProdutoDTO.java
@@ -19,70 +19,59 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProdutoDTO implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private Long id;
-	
-	@NotBlank(message = "nome é obrigatório")
-	private String nome;
-	
-	private String descricao;
-	
-	@NotBlank(message = "Codigo de referência é obrigatório")
-	private String codigoReferencia;
-	
-	@NotNull(message = "Informe a Unidade de medida")
-	private UnidadeMedida unidadeMedida;
-	
-	private String localizacaoFisica;
-	
-	@NotNull(message = "risco é obrigatório")
-	private NivelRisco risco;
-	
-	private TipoRisco tipoRisco;
-	
-	private String descricaoRisco;
-	
-	@NotNull(message = "Precisa confirmar se é perecivel")
-	private Boolean perecivel;
-	
-	private TipoPerecivel tipoPerecivel;
-	
-	private LocalDate dataValidade;
-	
-	private String condicoesArmazenamento;
 
-	private String unidadeArmazenamento;
+    private static final long serialVersionUID = 1L;
 
-	private Boolean ativo;
-	
-	public class DescarteProdutoDTO {
+    private Long id;
 
-	    @NotNull
-	    @Min(1)
-	    private Integer quantidade;
+    @NotBlank(message = "nome é obrigatório")
+    private String nome;
 
-	    @NotBlank
-	    private String justificativa;
-	}
-	
-	public ProdutoDTO(Produto entity) {
-		this.id = entity.getId();
-		this.nome = entity.getNome();
-		this.descricao = entity.getDescricao();
-		this.codigoReferencia = entity.getCodigoReferencia();
-		this.unidadeMedida = entity.getUnidadeMedida();
-		this.localizacaoFisica = entity.getLocalizacaoFisica();
-		this.risco = entity.getRisco();
-		this.tipoRisco = entity.getTipoRisco();
-		this.descricaoRisco = entity.getDescricaoRisco();
-		this.perecivel = entity.getPerecivel();
-		this.tipoPerecivel = entity.getTipoPerecivel();
-		this.dataValidade = entity.getDataValidade();
-		this.condicoesArmazenamento = entity.getCondicoesArmazenamento();
-		this.unidadeArmazenamento = entity.getUnidadeArmazenamento();
-		this.ativo = entity.getAtivo() != null ? entity.getAtivo() : true;
-	}
+    private String descricao;
 
+    @NotBlank(message = "Codigo de referência é obrigatório")
+    private String codigoReferencia;
+
+    @NotNull(message = "Informe a Unidade de medida")
+    private UnidadeMedida unidadeMedida;
+
+    private String localizacaoFisica;
+
+    @NotNull(message = "risco é obrigatório")
+    private NivelRisco risco;
+
+    private TipoRisco tipoRisco;
+
+    private String descricaoRisco;
+
+    @NotNull(message = "Precisa confirmar se é perecivel")
+    private Boolean perecivel;
+
+    private TipoPerecivel tipoPerecivel;
+
+    private LocalDate dataValidade;
+
+    private String condicoesArmazenamento;
+
+    private String unidadeArmazenamento;
+
+    private Boolean ativo;
+
+    public ProdutoDTO(Produto entity) {
+        this.id = entity.getId();
+        this.nome = entity.getNome();
+        this.descricao = entity.getDescricao();
+        this.codigoReferencia = entity.getCodigoReferencia();
+        this.unidadeMedida = entity.getUnidadeMedida();
+        this.localizacaoFisica = entity.getLocalizacaoFisica();
+        this.risco = entity.getRisco();
+        this.tipoRisco = entity.getTipoRisco();
+        this.descricaoRisco = entity.getDescricaoRisco();
+        this.perecivel = entity.getPerecivel();
+        this.tipoPerecivel = entity.getTipoPerecivel();
+        this.dataValidade = entity.getDataValidade();
+        this.condicoesArmazenamento = entity.getCondicoesArmazenamento();
+        this.unidadeArmazenamento = entity.getUnidadeArmazenamento();
+        this.ativo = entity.getAtivo() != null ? entity.getAtivo() : true;
+    }
 }
