f298a99e21f7c086348a03b271e5fefd23a953df
gbsalermo <enzogabrielskull@gmail.com>
2026-08-05 15:03:54 -0300

    Fix: ATUALIZAÇÃO DOS METODOS ENTRADA E SAIDA

--- src/main/java/com/sgl/dto/DescarteProdutoDTO.java
+++ src/main/java/com/sgl/dto/DescarteProdutoDTO.java
@@ -3,7 +3,11 @@ package com.sgl.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class DescarteProdutoDTO {
 
     @NotNull(message = "Quantidade é obrigatória")
@@ -25,27 +29,4 @@ public class DescarteProdutoDTO {
         this.usuarioId = usuarioId;
     }
 
-    public Integer getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(Integer quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public String getJustificativa() {
-        return justificativa;
-    }
-
-    public void setJustificativa(String justificativa) {
-        this.justificativa = justificativa;
-    }
-
-    public Long getUsuarioId() {
-        return usuarioId;
-    }
-
-    public void setUsuarioId(Long usuarioId) {
-        this.usuarioId = usuarioId;
-    }
 }
