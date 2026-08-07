e7edb779acbc28c55e1e3a94b835df339f2bf563
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-07 14:55:31 -0300

    fix: padronizar campo estoqueCentralId no lote DTO

--- src/main/java/com/sgl/dto/LoteDTO.java
+++ src/main/java/com/sgl/dto/LoteDTO.java
@@ -15,49 +15,47 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoteDTO {
-	
-	private Long id;
-	
-	private Long EstoqueCentralId;
-	
-	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	private Long produtoId;
-	
-	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	private String produtoNome;
 
-	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	private Long unidadeId;
+    private Long id;
 
-	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	private String unidadeNome;
-	
-	private String numeroLote;
-	
-	private Integer quantidadeInicial;
-	
-	private Integer quantidadeDisponivel;
-	
-	private LocalDate dataEntrada;
-	
-	private LocalDate dataValidade;
-	
-	private Boolean ativo;
-	
-	public LoteDTO(Lote entity) {
-		this.id = entity.getId();
-		this.EstoqueCentralId = entity.getEstoqueCentral().getId();
-		this.produtoId = entity.getEstoqueCentral().getProduto().getId();
-		this.produtoNome = entity.getEstoqueCentral().getProduto().getNome();
-		this.unidadeId = entity.getEstoqueCentral().getUnidade().getId();
-		this.unidadeNome = entity.getEstoqueCentral().getUnidade().getNome();
-		this.numeroLote = entity.getNumeroLote();
-		this.quantidadeInicial = entity.getQuantidadeInicial();
-		this.quantidadeDisponivel = entity.getQuantidadeDisponivel();
-		this.dataEntrada = entity.getDataEntrada();
-		this.dataValidade = entity.getDataValidade();
-		this.ativo = entity.getAtivo();
-		
-	}
-	
+    private Long estoqueCentralId;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long produtoId;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String produtoNome;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long unidadeId;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String unidadeNome;
+
+    private String numeroLote;
+
+    private Integer quantidadeInicial;
+
+    private Integer quantidadeDisponivel;
+
+    private LocalDate dataEntrada;
+
+    private LocalDate dataValidade;
+
+    private Boolean ativo;
+
+    public LoteDTO(Lote entity) {
+        this.id = entity.getId();
+        this.estoqueCentralId = entity.getEstoqueCentral().getId();
+        this.produtoId = entity.getEstoqueCentral().getProduto().getId();
+        this.produtoNome = entity.getEstoqueCentral().getProduto().getNome();
+        this.unidadeId = entity.getEstoqueCentral().getUnidade().getId();
+        this.unidadeNome = entity.getEstoqueCentral().getUnidade().getNome();
+        this.numeroLote = entity.getNumeroLote();
+        this.quantidadeInicial = entity.getQuantidadeInicial();
+        this.quantidadeDisponivel = entity.getQuantidadeDisponivel();
+        this.dataEntrada = entity.getDataEntrada();
+        this.dataValidade = entity.getDataValidade();
+        this.ativo = entity.getAtivo();
+    }
 }
