e7d979c5d4f728ea70a33ed010316a37eef9d950
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-07 15:12:23 -0300

    refactor: remover validade do produto dto

--- backend/sgl-backend/src/main/java/com/sgl/dto/ProdutoDTO.java
+++ backend/sgl-backend/src/main/java/com/sgl/dto/ProdutoDTO.java
@@ -1,7 +1,6 @@
 package com.sgl.dto;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 
 import com.sgl.model.Produto;
 import com.sgl.model.enums.NivelRisco;
@@ -9,7 +8,6 @@ import com.sgl.model.enums.TipoPerecivel;
 import com.sgl.model.enums.TipoRisco;
 import com.sgl.model.enums.UnidadeMedida;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -20,60 +18,56 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProdutoDTO implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private Long id;
-	
-	@NotBlank(message = "nome é obrigatório")
-	private String nome;
-	
-	private String descricao;
-	
-	@NotBlank(message = "Codigo de referência é obrigatório")
-	private String codigoReferencia;
-	
-	@NotNull(message = "Informe a Unidade de medida")
-	private UnidadeMedida unidadeMedida;
-	
-	private String localizacaoFisica;
-	
-	@NotNull(message = "risco é obrigatório")
-	private NivelRisco risco;
-	
-	private TipoRisco tipoRisco;
-	
-	private String descricaoRisco;
-	
-	@NotNull(message = "Precisa confirmar se é perecivel")
-	private Boolean perecivel;
-	
-	private TipoPerecivel tipoPerecivel;
-	
-	private LocalDate dataValidade;
-	
-	private String condicoesArmazenamento;
 
-	private String unidadeArmazenamento;
+    private static final long serialVersionUID = 1L;
 
-	private Boolean ativo;
-	
-	public ProdutoDTO(Produto entity) {
-		this.id = entity.getId();
-		this.nome = entity.getNome();
-		this.descricao = entity.getDescricao();
-		this.codigoReferencia = entity.getCodigoReferencia();
-		this.unidadeMedida = entity.getUnidadeMedida();
-		this.localizacaoFisica = entity.getLocalizacaoFisica();
-		this.risco = entity.getRisco();
-		this.tipoRisco = entity.getTipoRisco();
-		this.descricaoRisco = entity.getDescricaoRisco();
-		this.perecivel = entity.getPerecivel();
-		this.tipoPerecivel = entity.getTipoPerecivel();
-		this.dataValidade = entity.getDataValidade();
-		this.condicoesArmazenamento = entity.getCondicoesArmazenamento();
-		this.unidadeArmazenamento = entity.getUnidadeArmazenamento();
-		this.ativo = entity.getAtivo() != null ? entity.getAtivo() : true;
-	}
+    private Long id;
 
+    @NotBlank(message = "nome é obrigatório")
+    private String nome;
+
+    private String descricao;
+
+    @NotBlank(message = "Codigo de referência é obrigatório")
+    private String codigoReferencia;
+
+    @NotNull(message = "Informe a Unidade de medida")
+    private UnidadeMedida unidadeMedida;
+
+    private String localizacaoFisica;
+
+    @NotNull(message = "risco é obrigatório")
+    private NivelRisco risco;
+
+    private TipoRisco tipoRisco;
+
+    private String descricaoRisco;
+
+    @NotNull(message = "Precisa confirmar se é perecivel")
+    private Boolean perecivel;
+
+    private TipoPerecivel tipoPerecivel;
+
+    private String condicoesArmazenamento;
+
+    private String unidadeArmazenamento;
+
+    private Boolean ativo;
+
+    public ProdutoDTO(Produto entity) {
+        this.id = entity.getId();
+        this.nome = entity.getNome();
+        this.descricao = entity.getDescricao();
+        this.codigoReferencia = entity.getCodigoReferencia();
+        this.unidadeMedida = entity.getUnidadeMedida();
+        this.localizacaoFisica = entity.getLocalizacaoFisica();
+        this.risco = entity.getRisco();
+        this.tipoRisco = entity.getTipoRisco();
+        this.descricaoRisco = entity.getDescricaoRisco();
+        this.perecivel = entity.getPerecivel();
+        this.tipoPerecivel = entity.getTipoPerecivel();
+        this.condicoesArmazenamento = entity.getCondicoesArmazenamento();
+        this.unidadeArmazenamento = entity.getUnidadeArmazenamento();
+        this.ativo = entity.getAtivo() != null ? entity.getAtivo() : true;
+    }
 }
