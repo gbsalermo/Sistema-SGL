aa193ee96aae7cb44e54b56a647ff8ee2cdad341
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-07 15:12:38 -0300

    refactor: remover consulta de validade do produto

--- backend/sgl-backend/src/main/java/com/sgl/repository/ProdutoRepository.java
+++ backend/sgl-backend/src/main/java/com/sgl/repository/ProdutoRepository.java
@@ -1,34 +1,26 @@
 package com.sgl.repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sgl.model.Produto;
 import com.sgl.model.enums.NivelRisco;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-	
-	List<Produto> findByRisco(NivelRisco risco);
-	
-	List<Produto> findByPerecivelTrue();
-	
-	List<Produto> findByNomeContainingIgnoreCase(String nome);
-	
-	//Buscar produtos perecíveis com validade próxima
-	@Query("SELECT p FROM Produto p WHERE p.perecivel = true AND p.dataValidade BETWEEN :dataAtual And :dataLimite")
-	List<Produto> findPereciveisComValidadeProxima(@Param("dataAtual")
-	LocalDate dataAtual, @Param("dataLimite") LocalDate dataLimite);
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
-	boolean existsByCodigoReferencia(String codigoReferencia);
-	
-	boolean existsByCodigoReferenciaAndIdNot(
-	        String codigoReferencia,
-	        Long id
-	);
+    List<Produto> findByRisco(NivelRisco risco);
+
+    List<Produto> findByPerecivelTrue();
+
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
+
+    boolean existsByCodigoReferencia(String codigoReferencia);
+
+    boolean existsByCodigoReferenciaAndIdNot(
+            String codigoReferencia,
+            Long id
+    );
 }
