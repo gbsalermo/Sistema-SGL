e630d39a9dbb53310181c77db3356b89f85177d3
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-07 15:13:38 -0300

    refactor: remover autorizacao de produto vencido do pedido

--- src/main/java/com/sgl/dto/AprovarPedidoDTO.java
+++ src/main/java/com/sgl/dto/AprovarPedidoDTO.java
@@ -14,26 +14,24 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AprovarPedidoDTO {
 
-	private String observacao;
-	
-	@NotNull(message = "Lista de itens aprovados é obrigatoria")
-	private List<ItemAprovacaoDTO> itens;
-	
-	private Boolean autorizarProdutoVencido;
-	
-	@NotNull(message = "Id do usuário que aprovou é obrigatório")
-	private Long usuarioAprovadorId;
-	
-	
-	@Getter
-	@Setter
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class ItemAprovacaoDTO{
-		@NotNull(message = "Id do item é obrigatoria")
-		private Long itemId;
-		
-		@NotNull(message = "Quantidade aprovada é obrigatoria")
-		private Integer quantidadeAprovada;
-	}
+    private String observacao;
+
+    @NotNull(message = "Lista de itens aprovados é obrigatoria")
+    private List<ItemAprovacaoDTO> itens;
+
+    @NotNull(message = "Id do usuário que aprovou é obrigatório")
+    private Long usuarioAprovadorId;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ItemAprovacaoDTO {
+
+        @NotNull(message = "Id do item é obrigatoria")
+        private Long itemId;
+
+        @NotNull(message = "Quantidade aprovada é obrigatoria")
+        private Integer quantidadeAprovada;
+    }
 }
