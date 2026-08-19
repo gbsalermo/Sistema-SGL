5c3dac7a129a58606780fa529b8072558092cb0e
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-19 16:01:30 -0300

    refactor: move descarte de produto para request DTO

---
+++ src/main/java/com/sgl/dto/request/DescarteProdutoRequestDTO.java
@@ -0,0 +1,23 @@
+package com.sgl.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DescarteProdutoRequestDTO {
+
+    @NotNull(message = "Quantidade é obrigatória")
+    @Min(value = 1, message = "Quantidade deve ser no mínimo 1")
+    private Integer quantidade;
+
+    @NotBlank(message = "Justificativa é obrigatória")
+    private String justificativa;
+}
