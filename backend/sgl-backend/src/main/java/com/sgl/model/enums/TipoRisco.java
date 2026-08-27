b4c34a25535f60b87f648c93caaf89be5c106410
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-27 14:34:42 -0300

    feat: adiciona dominio e persistencia de residuos

--- backend/sgl-backend/src/main/java/com/sgl/model/enums/TipoRisco.java
+++ backend/sgl-backend/src/main/java/com/sgl/model/enums/TipoRisco.java
@@ -2,11 +2,16 @@ package com.sgl.model.enums;
 
 public enum TipoRisco {
 
-	NENHUM,
-	INFLAMAVEL,
-	RADIOATIVO,
-	TOXICO,
-	CORROSIVO,
-	BIOLOGICO
-	
+    NENHUM,
+    INFLAMAVEL,
+    RADIOATIVO,
+    TOXICO,
+    CORROSIVO,
+    BIOLOGICO,
+    IRRITANTE,
+    PERIGO_SAUDE,
+    OXIDANTE,
+    EXPLOSIVO,
+    GAS_PRESSURIZADO,
+    PERIGO_AMBIENTAL
 }
