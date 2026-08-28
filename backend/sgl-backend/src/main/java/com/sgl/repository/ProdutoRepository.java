ce69aa4a06fd565aafe978a3279f2ffebe985e02
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-28 09:02:03 -0300

    feat: adiciona bloqueio de produto para sequencia de lote

--- backend/sgl-backend/src/main/java/com/sgl/repository/ProdutoRepository.java
+++ backend/sgl-backend/src/main/java/com/sgl/repository/ProdutoRepository.java
@@ -5,15 +5,24 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sgl.model.Produto;
 import com.sgl.model.enums.NivelRisco;
 
+import jakarta.persistence.LockModeType;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	
 	Optional<Produto> findByPublicId(UUID publicId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Produto p WHERE p.id = :id")
+    Optional<Produto> buscarPorIdComBloqueio(@Param("id") Long id);
 
     List<Produto> findByRisco(NivelRisco risco);
 
