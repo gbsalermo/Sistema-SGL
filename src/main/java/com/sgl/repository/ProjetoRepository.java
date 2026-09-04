b317502c877bedffc6790839bd066677e799a09b
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-04 10:23:41 -0300

    feat(multitenant): filtrar projetos por unidade

--- src/main/java/com/sgl/repository/ProjetoRepository.java
+++ src/main/java/com/sgl/repository/ProjetoRepository.java
@@ -7,17 +7,17 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 import com.sgl.model.Projeto;
 
 @Repository
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
-	
-	Optional<Projeto> findByPublicId(UUID publicId);
-	
-	
-	List<Projeto> findByLaboratorioId(Long laboratorioId);
-	
-	List<Projeto> findByAtivoTrue();
 
+    Optional<Projeto> findByPublicId(UUID publicId);
+    Optional<Projeto> findByPublicIdAndLaboratorioUnidadePublicId(UUID publicId, UUID unidadePublicId);
+
+    List<Projeto> findByLaboratorioId(Long laboratorioId);
+    List<Projeto> findByLaboratorioUnidadePublicId(UUID unidadePublicId);
+
+    List<Projeto> findByAtivoTrue();
+    List<Projeto> findByLaboratorioUnidadePublicIdAndAtivoTrue(UUID unidadePublicId);
 }
