31366ce2736399af1215ab931e67e0951e0bff03
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-04 10:22:56 -0300

    feat(multitenant): filtrar laboratorios por unidade

--- src/main/java/com/sgl/repository/LaboratorioRepository.java
+++ src/main/java/com/sgl/repository/LaboratorioRepository.java
@@ -9,12 +9,12 @@ import org.springframework.stereotype.Repository;
 
 import com.sgl.model.Laboratorio;
 
-@Repository //Classe responsavel pela camada de persistencia
+@Repository
 public interface LaboratorioRepository extends JpaRepository<Laboratorio, Long> {
-	
-	Optional<Laboratorio> findByPublicId(UUID publicId);
-	
-	List<Laboratorio> findByUnidadeId(Long unidadeId);
-	
 
+    Optional<Laboratorio> findByPublicId(UUID publicId);
+    Optional<Laboratorio> findByPublicIdAndUnidadePublicId(UUID publicId, UUID unidadePublicId);
+
+    List<Laboratorio> findByUnidadeId(Long unidadeId);
+    List<Laboratorio> findByUnidadePublicId(UUID unidadePublicId);
 }
