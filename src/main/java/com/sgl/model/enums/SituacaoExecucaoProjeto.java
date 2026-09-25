aa04de0c03b944e5eb3dd1866b4209e27fa66c37
gbsalermo <enzogabrielskull@gmail.com>
2026-09-25 08:42:06 -0300

    feat: add enums StatusProjeto and SituacaoExecucaoProjeto

---
+++ src/main/java/com/sgl/model/enums/SituacaoExecucaoProjeto.java
@@ -0,0 +1,9 @@
+package com.sgl.model.enums;
+
+public enum SituacaoExecucaoProjeto {
+
+	NAO_INFORMADO,
+	EM_ANDAMENTO_NO_PRAZO,
+	EM_ANDAMENTO_ATRASADO,
+	EXECUCAO_CANCELADA
+}
---
+++ src/main/java/com/sgl/model/enums/StatusProjeto.java
@@ -0,0 +1,9 @@
+package com.sgl.model.enums;
+
+public enum StatusProjeto {
+	
+	ATIVO,
+	ENCERRADO_COM_AVALIACAO_PENDENTE,
+	CONCLUIDO
+
+}
